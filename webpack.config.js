8fc09f09b9c6665e3da1e079a74c807a2bffdcf2
krzysiu-wilk <xedanion16@gmail.com>
2020-05-08 08:47:36 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './js/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                'style-loader',
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+                'postcss-loader'
+            ],
+        }, ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+};
