6375dbbdabf2506f471145748fbe7f651b136239
krzysiu-wilk <xedanion16@gmail.com>
2020-05-10 23:16:39 +0200

    edit feature

--- js/components/PageHandler.js
+++ js/components/PageHandler.js
@@ -19,7 +19,9 @@ export default class PageHandler {
         this.init = this.init.bind(this);
         this.addItem = this.addItem.bind(this);
         this.removeItem = this.removeItem.bind(this);
+        this.editItem = this.editItem.bind(this);
     }
+
     init() {
         // form validation
         $('#add-product-form').validate({
@@ -49,10 +51,34 @@ export default class PageHandler {
             }
         });
 
+        $('#edit-product-form').validate({
+            rules: {
+                editName: {
+                    required: true,
+                    minlength: 1,
+                    maxlength: 50
+                },
+                editAmount: {
+                    required: true,
+                    min: 1
+                },
+                newUnit: {
+                    required: true,
+                }
+            },
+            messages: {
+                editName: "Provide right product name (1-50 signs)",
+                editAmount: "Provide valid amount",
+                newUnit: "Choose unit",
+            }
+        });
+
         // event handlers
         $('#add-product-form').on('submit', this.addItem);
         $(document).on('click', '.remove', this.removeItem);
+        $(document).on('click', '.edit', this.editItem);
     }
+
     addItem(e) {
         e.preventDefault();
 
@@ -86,7 +112,7 @@ export default class PageHandler {
             let generatedLi = generateListItem(name, amount, unit);
 
             // append li item using variables you got from form
-            $('#' + category + ' > .list-group').append(generatedLi);
+            $(`#${category} > .list-group`).append(generatedLi);
 
             switch(category) {
                 case 'vegetables':
@@ -109,6 +135,7 @@ export default class PageHandler {
             $('#add-product-form').trigger('reset');
         }
     }
+
     removeItem(e) {
         e.preventDefault();
         // find badge with amount and unit info
@@ -156,5 +183,77 @@ export default class PageHandler {
         $(e.target).parents('li').remove();
 
         storageUpdate(this.pageState);
+    }
+
+    editItem(e) {
+        $('#exampleModalCenter').modal('show');
+        $('#edit-product-form').submit({event: e, thisObj: this}, this.handleEditSubmit);
+    }
+
+    handleEditSubmit(e) {
+        console.log(e);
+        e.preventDefault();
+        const { event, thisObj } = e.data;
+        const prevListItem = $(event.target).parents('li')[0]; 
+        const currentCategory = $(event.target).parents('div.tab-pane').attr('id');
+        const oldAmount = $(event.target).parents('li').find('span#itemAmount').text();
+        const oldUnit = $(event.target).parents('li').find('span#itemUnit').text();
+        const position = $(event.target).parents('li').index();
+        console.log(position);
+
+        if ($('#edit-product-form').valid()) {
+            const newName = $('#editName').val();
+            const newAmount = $('#editAmount').val();
+            const newUnit = $("input[name='newUnit']:checked").val();
+            let newListElement = generateListItem(newName, newAmount, newUnit);
+
+            $(prevListItem).replaceWith(newListElement);
+
+            switch(currentCategory) {
+                case 'vegetables':
+                    thisObj.pageState.vegetables[position] = newListElement;
+                    break;
+                case 'fruits':
+                    thisObj.pageState.fruits[position] = newListElement;
+                    break;
+                case 'dairy':
+                    thisObj.pageState.dairy[position] = newListElement;
+                    break;
+                case 'bread':
+                    thisObj.pageState.bread[position] = newListElement;
+                    break;
+                case 'hygiene':
+                    thisObj.pageState.hygiene[position] = newListElement;
+                    break;
+            }
+
+            switch (oldUnit) {
+                case 'pcs':
+                    thisObj.pageState.pieces -= parseInt(oldAmount);
+                    $('#pieces-value').text(thisObj.pageState.pieces);
+                    break;
+                case 'g':
+                    thisObj.pageState.grams -= parseInt(oldAmount);
+                    $('#grams-value').text(thisObj.pageState.grams);
+                    break;
+            }
+
+            switch (newUnit) {
+                case 'pcs':
+                    thisObj.pageState.pieces += parseInt(newAmount);
+                    $('#pieces-value').text(thisObj.pageState.pieces);
+                    break;
+                case 'g':
+                    thisObj.pageState.grams += parseInt(newAmount);
+                    $('#grams-value').text(thisObj.pageState.grams);
+                    break;
+            }
+
+            storageUpdate(thisObj.pageState);
+            $('#edit-product-form').trigger('reset');
+            $('#exampleModalCenter').modal('hide');
+        }
+
+        $('#edit-product-form').off("submit");
     }
 }
--- js/components/listElementGenerator.js
+++ js/components/listElementGenerator.js
@@ -1,5 +1,5 @@
 const generateListItem = (name, amount, unit) => {
-    return '<li class="list-group-item border-red mt-2">' + name + '<span class="badge red-scheme ml-1"><span id="itemAmount">' + amount + '</span><span class="ml-1" id="itemUnit">' + unit + '</span></span><div class="my-1 dark-red-text"><i class="fas fa-edit"></i><i class="ml-2 fas fa-ban mx-2 remove"></i><i class="fas fa-file-export"></i></div></li>';
+    return '<li class="list-group-item border-red mt-2">' + name + '<span class="badge red-scheme ml-1"><span id="itemAmount">' + amount + '</span><span class="ml-1" id="itemUnit">' + unit + '</span></span><div class="my-1 dark-red-text"><span class="edit"><i class="fas fa-edit"></i></span><i class="ml-2 fas fa-ban mx-2 remove"></i></div></li>';
 }
 
 export { generateListItem };
